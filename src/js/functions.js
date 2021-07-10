6f618238cc065c4f64c7d29a494782029964b99a
Cassiano - Manjaro <rodrigues.cassiano2007@gmail.com>
2021-07-10 10:11:40 -0300

    feat: adding resize function

--- src/js/functions.js
+++ src/js/functions.js
@@ -15,3 +15,49 @@ function formattingExpression(input) {
     .replace(/\ln/gi, "log")
     .replace(/\e/gi, "Math.E");
 }
+
+/** 
+ * Builds visual matrix in html
+ * 
+ * */
+const resize = () => {
+  const n = parseInt(document.querySelector("#n").value);
+  const context = document.querySelector("#config-matrix");
+  context.innerHTML = null;
+
+  if (n && n > 0) {
+    let row = document.createElement(`tr`);
+    let cell = document.createElement(`th`);
+    cell.innerText = `A`;
+    cell.className = "matrix-label";
+    row.appendChild(cell);
+
+    for (let i = 1; i <= n; i++) {
+      cell = document.createElement(`th`);
+      cell.innerText = `${i}`;
+      cell.className = "matrix-label";
+      row.appendChild(cell);
+    }
+    cell = document.createElement(`th`);
+    cell.innerText = `B`;
+    cell.className = "matrix-label";
+    row.appendChild(cell);
+    context.appendChild(row);
+
+    for (let i = 1; i <= n; i++) {
+      let row = document.createElement(`tr`);
+      cell = document.createElement(`th`);
+      cell.innerText = `${i}`;
+      row.appendChild(cell);
+      for (let j = 1; j <= n; j++) {
+        cell = document.createElement(`th`);
+        cell.innerHTML = `<input type="number" id="a${i}${j}">`;
+        row.appendChild(cell);
+      }
+      cell = document.createElement(`th`);
+      cell.innerHTML = `<input type="number" id="b${i}">`;
+      row.appendChild(cell);
+      context.appendChild(row);
+    }
+  }
+};
