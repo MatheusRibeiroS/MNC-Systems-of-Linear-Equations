62ecb5fdc8d49a18b3c5944f286f83276ae9125b
Matheus Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2021-07-10 14:11:18 -0300

    LU Method implemented

--- src/js/methods.js
+++ src/js/methods.js
@@ -170,4 +170,46 @@ const CompactGauss = (A, b) => {
     }
 };
 
+const LU = (A, b) => {
+    let L = [];
+    for (let i = 0; i < n; i++) {
+        L[i] = new Array(n);
+    }
+    for (let i = 0; i < n; i++) {
+        L[i][i] = 1;
+    }
+    for (let i = 0; i < n; i++) {
+        let pivo = A[i][i];
+        if (pivo == 0) { // o sistema é impossível de ser resolvido pelo método LU pois o pivo é zero
+            return;
+        }
+        for (let j = i + 1; j < n; j++) {
+            let a = A[j][i] / pivo;
+            for (let k = i; k < n; k++) {
+                A[j][k] -= A[i][k] * a;
+            }
+            L[j][i] = a;
+        }
+    }
+    let c = new Array(n);
+    for (let i = 0; i < n; i++) {
+        let aux = b[i];
+        for (let j = i - 1; j > -1; j--) {
+            aux -= c[j] * L[i][j];
+        }
+        c[i] = aux;
+    }
+    let x = new Array(n);
+    for (let i = n - 1; i > -1; i--) {
+        let aux = c[i];
+        for (let j = i + 1; j < n; j++) {
+            aux -= A[i][j] * x[j];
+        }
+        x[i] = aux / A[i][i];
+    }
+    for (let i = 1; i <= n; i++) {
+        // console.log();
+    }
+};
+
 
