30791c956446a1aa95a7ef8642d2ba4292087379
Matheus Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2021-07-10 14:03:14 -0300

    Compact Gauss Method implemented

--- src/js/methods.js
+++ src/js/methods.js
@@ -2,7 +2,7 @@ const simpleGauss = (A, b) => {
 
     for (let i = 0; i < n; i++) {
         let pivo = A[i][i];
-        if (pivo == 0) { // o sistema é impossível de ser resolvido pelo método de gauss pois o pivo é zero
+        if (pivo == 0) { // o sistema é impossível de ser resolvido pelo método de gauss simples pois o pivo é zero
             return;
         }
         for (let j = i + 1; j < n; j++) {
@@ -135,7 +135,39 @@ const TotalPivoGauss = (A, b) => {
     }
     for (let i = 1; i <= n; i++) {
         //  console.log();
-}
+    }
+};
+
+const CompactGauss = (A, b) => {
+    for (let i = 0; i < n; i++) {
+        let pivo = A[i][i];
+        if (pivo == 0) { // o sistema é impossível de ser resolvido pelo método de gauss compacto pois o pivo é zero
+            return;
+        }
+        for (let j = i + 1; j < n; j++) {
+            let a = A[j][i] / pivo;
+            A[j][i] = a;
+            for (let k = i + 1; k < n; k++) {
+                A[j][k] -= A[i][k] * a;
+            }
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        for (let j = i - 1; j > -1; j--) {
+            b[i] -= b[j] * A[i][j];
+        }
+    }
+    let x = new Array(n);
+    for (let i = n - 1; i > -1; i--) {
+        let aux = b[i];
+        for (let j = i + 1; j < n; j++) {
+            aux -= A[i][j] * x[j];
+        }
+        x[i] = aux / A[i][i];
+    }
+    for (let i = 1; i <= n; i++) {
+        // console.log();
+    }
 };
 
 
