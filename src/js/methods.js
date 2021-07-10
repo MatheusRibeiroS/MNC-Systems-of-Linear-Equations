9a99e0c5b573d2b0090d655864de8fa296fb4cc9
Matheus Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2021-07-10 13:38:23 -0300

    Simple Gauss and Partial Pivo Gauss Methods implemented

--- src/js/methods.js
+++ src/js/methods.js
@@ -1,69 +1,75 @@
 const simpleGauss = (A, b) => {
 
-  for (let i = 0; i < n; i++) {
-    let pivo = A[i][i];
-    for (let j = i + 1; j < n; j++) {
-      let a = A[j][i] / pivo;
-      for (let k = i; k < n; k++) {
-        A[j][k] -= A[i][k] * a;
-      }
-      b[j] -= b[i]*a;
+    for (let i = 0; i < n; i++) {
+        let pivo = A[i][i];
+        if (pivo == 0) { // o sistema é impossível de ser resolvido pelo método de gauss pois o pivo é zero
+            return;
+        }
+        for (let j = i + 1; j < n; j++) {
+            let a = A[j][i] / pivo;
+            for (let k = i; k < n; k++) {
+                A[j][k] -= A[i][k] * a;
+            }
+            b[j] -= b[i] * a;
+        }
     }
-  }
-  let x = [];
-  for (let i = n - 1; i > -1; i--) {
-    let aux = b[i];
-    for (let j = i + 1; j < n; j++) {
-      aux -= A[i][j] * x[j];
+    let x = [];
+    for (let j = 0; j < n; j++) {
+        x.push(null);
     }
-    x[i] = aux / A[i][i];
-  }
+
+    for (let i = n - 1; i > -1; i--) {
+        let aux = b[i];
+        for (let j = i + 1; j < n; j++) {
+            aux -= A[i][j] * x[j];
+        }
+        x[i] = aux / A[i][i];
+    }
+    for (let i = 1; i <= n; i++) {
+        // console.log();
+    }
 };
 
-const cholesky = (A, b) => {
-  for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      if (A[i][j] != A[j][i]) {
-        // a matriz não é simétrica
-        return;
-      }
+const PartialPivoGauss = (A, b) => {
+    for (let i = 0; i < n; i++) {
+        let line = i;
+        for (let j = i + 1; j < n; j++) {
+            if (Math.abs(A[j][i]) > Math.abs(A[line][i])) {
+                line = j;
+            }
+        }
+        if (line != i) {
+            let aux = b[i];
+            b[i] = b[line];
+            b[line] = aux;
+            for (let k = i; k < n; k++) {
+                aux = A[line][k];
+                A[line][k] = A[i][k];
+                A[i][k] = aux;
+            }
+        }
+        for (let j = i + 1; j < n; j++) {
+            let m = A[j][i] / A[i][i];
+            for (let k = i; k < n; k++) {
+                A[j][k] -= A[i][k] * m;
+            }
+            b[j] -= b[i] * m;
+        }
     }
-  }
-  let L = [];
-  for (let i = 0; i < n; i++) {
-    L[i] = new Array(n);
-  }
-  for (let i = 0; i < n; i++) {
-    L[i][i] = 1;
-  }
-  for (let i = 0; i < n; i++) {
-    let pivo = A[i][i];
-    if (pivo == 0) {
-      // o sistema é impossível de ser resolvido pelo método de cholesky
-      return;
+    let x = [];
+    for (let i = 0; i < n; i++) {
+        x.push(null);
     }
-    for (let j = i + 1; j < n; j++) {
-      let a = A[j][i] / pivo;
-      for (let k = i; k < n; k++) {
-        A[j][k] -= A[i][k] * a;
-      }
-      L[j][i] = a;
+    for (let i = n - 1; i > -1; i--) {
+        let aux = b[i];
+        for (let j = i + 1; j < n; j++) {
+            aux -= A[i][j] * x[j];
+        }
+        x[i] = aux / A[i][i];
     }
-  }
-  let z = new Array(n);
-  for (let i = 0; i < n; i++) {
-    let aux = b[i];
-    for (let j = i - 1; j > -1; j--) {
-      aux -= z[j] * L[i][j];
+    for (let i = 1; i <= n; i++) {
+        // console.log();
     }
-    z[i] = aux;
-  }
-  let x = new Array(n);
-  for (let i = n - 1; i > -1; i--) {
-    let aux = z[i];
-    for (let j = i + 1; j < n; j++) {
-      aux -= A[i][j]*x[j];
-    }
-    x[i] = aux/A[i][i];
-  }
 };
+
+
